3bf03f15d7978bd2515bcdc01e00ea7647df3757
Aaron Keohane <aaronkeohane@gmail.com>
2021-12-03 17:18:04 -0800

    Initialized React Trivia App using create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <button> Get your trivia on!</button>
+    </div>
+  )
+}
