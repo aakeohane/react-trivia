256511978fb1f9c505fb7063b0cdb1778af7ddbc
Aaron Keohane <aaronkeohane@gmail.com>
2021-12-05 01:12:21 -0800

    styled the landing page and added questions and answers upon start quiz btn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
+
+  const [quiz, setQuiz] = useState(false)
+  const [quizData, setQuizData] = useState([])
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then(res => res.json())
+      .then(data => setQuizData(data.results))
+  }, [])
+  
+  const startQuiz = () => {
+    setQuiz(true)
+  }
+
+  // Fisher-Yates Shuffle Algorithm
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  const quizQuestions = quizData.map(item => {
+    // had to remove some of the JSON symbols since API kept them in data
+    const unicodeQ = item.question
+    const questions = unicodeQ.replace(/&quot;/g,'"').replace(/&#039;/g, "'").replace(/&amp;/g, "&")
+
+    const wrongAnswers = item.incorrect_answers
+    wrongAnswers.push(item.correct_answer)
+    console.log(item.correct_answer)
+    const answersArray = [...new Set(wrongAnswers)]
+    shuffle(answersArray)
+    const answers = answersArray.map(item => {
+        const unicodeA = item
+        const fixedAnswers = unicodeA.replace(/&quot;/g,'"').replace(/&#039;/g, "'").replace(/&amp;/g, "&")
+        return <li className="answers">{fixedAnswers}</li>
+      })
+
+    return (
+      <div>
+        <p>{questions}</p>
+        <ul>{answers}</ul>
+      </div>
+      
+    )
+  })
+
   return (
     <div className="App">
-      <LandingPage />
+      <div className="landing-container">
+        <div className="yellow-blob"></div>
+        <div className="blue-blob"></div>
+        { !quiz && <LandingPage onStart={() => startQuiz()} />}
+        <div className="question-container">
+          { quiz && quizQuestions}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function LandingPage() {
+export default function LandingPage(props) {
+
+  
+
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <button> Get your trivia on!</button>
+    <div className="content">
+      <h1>Quizzical</h1>
+      <p> Test your knowledge! When you start the quiz, you will have to answer 5 random questions. Good luck!</p>
+      <button className="quizzical-button" onClick={props.onStart}> Start quiz!</button>
     </div>
   )
 }
